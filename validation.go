2d71602b59585206e0caa853fe1707e5de191f69
YasinSaee <yasinvsaee@gmail.com>
2024-03-15 17:55:58 +0330

    validation package

---
+++ validation.go
@@ -0,0 +1,22 @@
+package iranian_tools
+
+import (
+	"github.com/YasinSaee/iranian_tools/util/validators/national"
+	"github.com/YasinSaee/iranian_tools/util/validators/phone"
+	"github.com/YasinSaee/iranian_tools/util/validators/post"
+)
+
+// چک کردن شماره تلفن های ایرانی
+func CheckCellPhone(cellPhone string) bool {
+	return phone.CellphoneValidator(cellPhone)
+}
+
+// چک کردن کد پستی های ایرانی
+func CheckPostalCode(postalCode string) bool {
+	return post.PostalCodeValidator(postalCode)
+}
+
+// چک کردن کد ملی های ایرانی
+func CheckNationalCode(nationalCode string) bool {
+	return national.NationalCodeValidator(nationalCode)
+}
